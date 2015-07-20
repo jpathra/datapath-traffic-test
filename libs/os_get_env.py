8f5413056f7b359db6529f8312129a1d5de33712
Sabdhagiri Govindaraju <sabdhagiri@onecloudinc.com>
2015-07-20 04:44:07 -0700

    code to get l3, l2 policy and ptgs

--- libs/os_get_env.py
+++ libs/os_get_env.py
@@ -1,18 +1,55 @@
 import os
 import sys
 import logging
-from keystoneclient.v2_0 import client as ks_client
-
+from keystoneclient.v2_0 import client as ksc
+from gbpclient.v2_0 import client as gbpc
 
 class OSUtils(object):
     
     def __init__(self, ks_user, ks_pass, ks_auth_url, ks_tenant_name):
         self.logger = logging.getLogger(__name__)
-        self.ks = ks_client.Client(username=ks_user, password = ks_pass, tenant_name = ks_tenant_name, auth_url = ks_auth_url)
+        self.ks_user = ks_user
+        self.ks_pass = ks_pass
+        self.ks_auth_url = ks_auth_url
+        self.ks = ksc.Client(username=ks_user, password = ks_pass, tenant_name = ks_tenant_name, auth_url = ks_auth_url)
+        self.logger.info('OSUtils intialized successfully')
+
+    def get_gbp_client_by_tenant(self, tenant):
+        self.logger.info('get gbpclient object for tenant')
+        gbpclient = gbpc.Client(username = self.ks_user, password = self.ks_pass, tenant_name = tenant, auth_url = self.ks_auth_url)
+        return gbpclient
 
     def get_tenants_list(self):
+        self.logger.info('Get tenant list from Keystone')
         tenants = self.ks.tenants.list()
-        self.logger.info(tenants)
-        return tenants
+        self.logger.info('\n*\n*\t\tTenants\n*\n')
+        self.logger.info('*'*50)
+        return { tenant.id : tenant.name for tenant in tenants if tenant.enabled and tenant.name != 'services'}
     
+    def get_gbp_l3policies_by_tenant(self, tenant):
+        gbpclient = self.get_gbp_client_by_tenant(tenant[1])
+        l3policies = gbpclient.list_l3_policies()['l3_policies']
+        l3policy_list_by_tenant = [ l3p for l3p in l3policies if l3p['tenant_id'] == tenant[0] ]
+        self.logger.info('\n*\n*\t\tL3Policies for %s\n*' %(tenant[1]))
+        for l3policy in l3policy_list_by_tenant:
+            self.logger.info(l3policy['name'])
+        return l3policy_list_by_tenant
+      
 
+    def get_gbp_l2policies_by_tenant(self, tenant):
+        gbpclient = self.get_gbp_client_by_tenant(tenant[1])
+        l2policies = gbpclient.list_l2_policies()['l2_policies']
+        l2policy_list_by_tenant = [ l2p for l2p in l2policies if l2p['tenant_id'] == tenant[0] ]
+        self.logger.info('\n*\n*\t\tL2Policies for %s\n*' %(tenant[1]))
+        for l2policy in l2policy_list_by_tenant:
+            self.logger.info(l2policy['name'])
+        return l2policy_list_by_tenant
+
+    def get_gbp_ptgs_by_tenant(self, tenant):
+        gbpclient = self.get_gbp_client_by_tenant(tenant[1])
+        ptgs = gbpclient.list_policy_target_groups()['policy_target_groups']
+        ptg_list_by_tenant = [ ptg for ptg in ptgs if ptg['tenant_id'] == tenant[0] ]
+        self.logger.info('\n*\n*\t\tPloicy Target Groups for %s\n*' %(tenant[1]))
+        for ptg in ptg_list_by_tenant:
+            self.logger.info(ptg['name'])
+        return ptg_list_by_tenant
--- main.py
+++ main.py
@@ -9,16 +9,19 @@ from libs import os_get_env as os_lib
 logging.config.fileConfig('conf/logging.ini')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-#handler = logging.FileHandler('dptest.log')
-#handler.setLevel(logging.DEBUG)
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#handler.setFormatter(formatter)
-#logger.addHandler(handler)
+def main():
+    print 'I am here'
+    logger.info('Start the program')
+    logger.info('Initialize OSUtils')
 
-logger.info('Start the program')
-osutils = os_lib.OSUtils(os_cfg.keystone_user, os_cfg.keystone_password, os_cfg.keystone_auth_url, os_cfg.keystone_tenant_name)
-tenants = osutils.get_tenants_list()
-for tenant in tenants:
-    print tenant.name
+    osutils = os_lib.OSUtils(os_cfg.keystone_user, os_cfg.keystone_password, os_cfg.keystone_auth_url, os_cfg.keystone_tenant_name)
+    logger.info('Get tenant details')
+    tenants = osutils.get_tenants_list()
+    logger.info('Get GBP L3 policies per tenant')
+    for tenant in tenants.items(): 
+        osutils.get_gbp_l3policies_by_tenant(tenant)
+        osutils.get_gbp_l2policies_by_tenant(tenant)
+        osutils.get_gbp_ptgs_by_tenant(tenant)
+    logger.info('end of main')
 
-logger.info('end of main')
+main()
